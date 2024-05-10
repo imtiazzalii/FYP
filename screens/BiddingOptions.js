69da5a9d0ceb6d6fd75ba0dc9e00b8f38f81fb79
Imtiaz Ali <imtiaz.mushfiq01@gmail.com>
2024-05-10 16:17:05 +0500

    biddingoptions UI adjusted

--- screens/BiddingOptions.js
+++ screens/BiddingOptions.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   RefreshControl,
   ScrollView,
+  StyleSheet,
   ImageBackground,
 } from "react-native";
 import tw from "twrnc";
@@ -49,85 +50,95 @@ const BiddingOptions = () => {
 
   const updateBidStatus = async (bidId, newStatus) => {
     try {
-        // Update local state first
-        const updatedBids = bidsInfo.map(bid => {
-            if (bid._id === bidId) {
-                return { ...bid, status: newStatus };
-            }
-            return bid;
-        });
-        setBidsInfo(updatedBids);
-        
-        // Update bid status in the backend
-        await axios.put(Constants.expoConfig.extra.IP_ADDRESS + `/updateBidStatus/${bidId}`, { status: newStatus });
+      // Update local state first
+      const updatedBids = bidsInfo.map((bid) => {
+        if (bid._id === bidId) {
+          return { ...bid, status: newStatus };
+        }
+        return bid;
+      });
+      setBidsInfo(updatedBids);
 
-        let message, notificationType;
-        const bid = bidsInfo.find(bid => bid._id === bidId);
-        const BidderId = bid.bidderId;
+      // Update bid status in the backend
+      await axios.put(
+        Constants.expoConfig.extra.IP_ADDRESS + `/updateBidStatus/${bidId}`,
+        { status: newStatus }
+      );
 
-        if (newStatus === 'accepted') {
-            message = 'Your bid has been accepted, please proceed to make payment.';
-            notificationType = 'Accept';
+      let message, notificationType;
+      const bid = bidsInfo.find((bid) => bid._id === bidId);
+      const BidderId = bid.bidderId;
 
-            // Fetch the bid details including receiver information
-            const bidDetailsResponse = await axios.get(`${Constants.expoConfig.extra.IP_ADDRESS}/getBidById/${bidId}`);
-            const { recvName, recvNumber, recvCnic, tripId } = bidDetailsResponse.data.data;
-              console.log("Receiver Name:", recvName);
-              console.log("Receiver Number:", recvNumber);
-              console.log("Receiver CNIC:", recvCnic);
-              console.log("Trip ID:", tripId);
+      if (newStatus === "accepted") {
+        message = "Your bid has been accepted, please proceed to make payment.";
+        notificationType = "Accept";
 
-            // Update the trip details with receiver information
-            await axios.put(`${Constants.expoConfig.extra.IP_ADDRESS}/updateTripDetails/${tripId}`, {
-                recvName,
-                recvNumber,
-                recvCnic
-            });
+        // Fetch the bid details including receiver information
+        const bidDetailsResponse = await axios.get(
+          `${Constants.expoConfig.extra.IP_ADDRESS}/getBidById/${bidId}`
+        );
+        const { recvName, recvNumber, recvCnic, tripId } =
+          bidDetailsResponse.data.data;
+        console.log("Receiver Name:", recvName);
+        console.log("Receiver Number:", recvNumber);
+        console.log("Receiver CNIC:", recvCnic);
+        console.log("Trip ID:", tripId);
 
-            await axios.put(
-              `${Constants.expoConfig.extra.IP_ADDRESS}/updateTripStatus/${tripId}`,
-              {status: newStatus}
-            );
-             
-        console.log("BIDDER ID: ",BidderId)
-        console.log("BIDDER amnt: ",bidDetailsResponse.data.data.bid,)
+        // Update the trip details with receiver information
+        await axios.put(
+          `${Constants.expoConfig.extra.IP_ADDRESS}/updateTripDetails/${tripId}`,
+          {
+            recvName,
+            recvNumber,
+            recvCnic,
+          }
+        );
 
+        await axios.put(
+          `${Constants.expoConfig.extra.IP_ADDRESS}/updateTripStatus/${tripId}`,
+          { status: newStatus }
+        );
+
+        console.log("BIDDER ID: ", BidderId);
+        console.log("BIDDER amnt: ", bidDetailsResponse.data.data.bid);
 
         // Charge the wallet only when the bid is accepted
-        await axios.post(Constants.expoConfig.extra.IP_ADDRESS + '/chargeWallet', {
-          bidderId: BidderId,
-          bidAmount: bidDetailsResponse.data.data.bid,
-        });
+        await axios.post(
+          Constants.expoConfig.extra.IP_ADDRESS + "/chargeWallet",
+          {
+            bidderId: BidderId,
+            bidAmount: bidDetailsResponse.data.data.bid,
+          }
+        );
 
-        
-  
         // Create a friendship
-        await axios.post(Constants.expoConfig.extra.IP_ADDRESS + '/makeFriend', {
-          bidderId:BidderId,
-          userId: await AsyncStorage.getItem("userId")
-        });
+        await axios.post(
+          Constants.expoConfig.extra.IP_ADDRESS + "/makeFriend",
+          {
+            bidderId: BidderId,
+            userId: await AsyncStorage.getItem("userId"),
+          }
+        );
+      } else if (newStatus === "rejected") {
+        message = "Your bid has been rejected.";
+        notificationType = "Reject";
+      }
 
-
-        } else if (newStatus === 'rejected') {
-            message = 'Your bid has been rejected.';
-            notificationType = 'Reject';
-        }
-        
-       
-        if (bid) {
-        
-          // Send notification
-          await axios.post(Constants.expoConfig.extra.IP_ADDRESS + '/createNotification', {
+      if (bid) {
+        // Send notification
+        await axios.post(
+          Constants.expoConfig.extra.IP_ADDRESS + "/createNotification",
+          {
             userId: BidderId,
             message,
-            type: notificationType
-          });
-        }
-      } catch (error) {
-        console.error("Error in updateBidStatus:", error);
+            type: notificationType,
+          }
+        );
+      }
+    } catch (error) {
+      console.error("Error in updateBidStatus:", error);
     }
-   };
-
+  };
 
   return (
     <ImageBackground
@@ -140,44 +151,72 @@ const BiddingOptions = () => {
         }
         contentContainerStyle={tw`p-4 mt-10`}
       >
-        {bidsInfo
-          .filter((item) => item.status === "pending")
-          .map((item, index) => (
-            <View
-              key={index}
-              style={tw`flex-row items-center justify-between p-3 bg-white my-2 rounded-lg shadow`}
-            >
-              <Image
-                source={{ uri: item.bidderProfilePic }}
-                style={tw`w-12 h-12 rounded-full`}
-              />
-              <View>
-                <Text style={tw`font-bold`}>{item.bidderName}</Text>
-                <View style={tw`flex-row items-center`}>
-                  <Text>{`⭐ ${item.rating}`}</Text>
-                  <Text style={tw`ml-2`}>{item.capacity}kg</Text>
+        {bidsInfo.filter((item) => item.status === "pending").length > 0 ? (
+          bidsInfo
+            .filter((item) => item.status === "pending")
+            .map((item, index) => (
+              <View
+                key={index}
+                style={tw`flex-row items-center justify-between p-4 bg-white my-2 rounded-lg shadow`}
+              >
+                <Image
+                  source={{ uri: item.bidderProfilePic }}
+                  style={tw`w-12 h-12 rounded-full`}
+                />
+                <View style={styles.textContainer}>
+                  <Text style={tw`font-bold`}>{item.bidderName}</Text>
+                  <View style={tw`flex-row items-center`}>
+                    <Text>{`⭐ ${item.rating}`}</Text>
+                    <Text style={tw`ml-2`}>{item.capacity}kg</Text>
+                  </View>
+                </View>
+                <View style={styles.bidContainer}>
+                  <Text style={tw`font-bold`}>Rs. {item.bid}</Text>
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                      style={tw`bg-red-400 px-3 py-1 rounded-full mr-2`}
+                      onPress={() => updateBidStatus(item._id, "rejected")}
+                    >
+                      <Text style={tw`text-white text-xs`}>Decline</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={tw`bg-green-500 px-3 py-1 rounded-full`}
+                      onPress={() => updateBidStatus(item._id, "accepted")}
+                    >
+                      <Text style={tw`text-white text-xs`}>Accept</Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-              <Text style={tw`font-bold`}>Rs. {item.bid}</Text>
-              <View style={tw`flex-row`}>
-                <TouchableOpacity
-                  style={tw`bg-red-400 px-3 py-1 rounded-full mr-2`}
-                  onPress={() => updateBidStatus(item._id, "rejected")}
-                >
-                  <Text style={tw`text-white text-xs`}>Decline</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={tw`bg-green-500 px-3 py-1 rounded-full`}
-                  onPress={() => updateBidStatus(item._id, "accepted")}
-                >
-                  <Text style={tw`text-white text-xs`}>Accept</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          ))}
+            ))
+        ) : (
+          <Text style={styles.noBidsText}>No bids available</Text>
+        )}
       </ScrollView>
     </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  noBidsText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 20,
+    color: "#47ADB8",
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  bidContainer: {
+    flexDirection: "column",
+    alignItems: "flex-end",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 5,
+  },
+});
 
 export default BiddingOptions;
